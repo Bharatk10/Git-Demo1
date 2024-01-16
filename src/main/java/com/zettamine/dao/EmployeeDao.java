cfd4896c07df4373cdfca15630cccf79d466e9ae
and <bharatkumat84101@gmail.com>
2024-01-16 14:48:40 +0530

    Added Employee Dao Methods

--- src/main/java/com/zettamine/dao/EmployeeDao.java
+++ src/main/java/com/zettamine/dao/EmployeeDao.java
@@ -1,7 +1,13 @@
 package com.zettamine.dao;
 
+import java.util.List;
+
+import com.zettamine.model.Employee;
+
 public interface EmployeeDao {
 	
+	List<Employee> getAllEmployees();
 	
+	Employee getEmployeeById(int id);
 
 }
